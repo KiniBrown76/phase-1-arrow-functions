c265fa2ea2f8fe717cf17c7a08f458a0b3cf1073
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-06-25 13:27:07 +0300

    Add arrow

---
+++ index.js
@@ -0,0 +1,15 @@
+// function expression called divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow Function called square
+  const square = (F) => {
+    return F * F; 
+  };
+
+  //Arrow function called add
+  const add = (r, j) => {
+    return r + j;
+  };
+
